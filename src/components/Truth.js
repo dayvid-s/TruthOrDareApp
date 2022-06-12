d1274f4e31e3220527a26b3a371fc10a05abd85a
Dayvid <@dayvid7santos2002@outlook.com>
2022-06-12 15:17:15 -0300

    Update Truth.js

--- src/components/Truth.js
+++ src/components/Truth.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { View,Text, StyleSheet, TouchableOpacity } from 'react-native';
-import Challengues from './Challengues';
 import Icon  from 'react-native-vector-icons/AntDesign';
+import InitialPart from './InitialPart';
 
 
-export default function Truth({ navigation: { goBack } }) {
-    return( 
+export default class extends Component  {
+  state= {
+    ChallenguesDone: ['What would you like to do with a partner if you could erase her memory?'
+    ,'eae  man'],
+    ChallenguesUndone:['']
+  }
+  alternar=()=>{
+    this.setState({
+    ChallenguesDone:['vish']    
+})
+}
+render(){
+  return(
         <View style={styles.container} >
             <View style={{height: ('5%')}} > 
               <Text style={styles.lettersOne}  >Dayvid</Text>
-              <TouchableOpacity onPress={() => goBack()}  >
+              <TouchableOpacity onPress={() => navigation.navigate(InitialPart)}  >
                  <Icon name='left' size={30} color='#ff09de'/>
               </TouchableOpacity>
               <Text style={styles.lettersTwo} >HARD</Text>
@@ -22,13 +33,17 @@ export default function Truth({ navigation: { goBack } }) {
                </Text>    
              </View>
              <Text style={{  alignSelf:'center',color:('#De2674')}} >  ______________</Text>
-             <Challengues></Challengues>
+             <View>
+               <Text style={{fontSize:28,textAlign:'center'}} >{ this.state.ChallenguesDone} </Text>
+            </View>
              <Text style={{  alignSelf:'center',color:('#De2674')}} >  ______________</Text>
              </View>
            
            <View style={{flexDirection:'row', alignSelf:'center', justifyContent:'space-between'  } } >
               <View style={{right:20}}  >
+                <TouchableOpacity onPress={this.alternar}>
                 <Icon name='reload1' size={70} color='#ff09de' />
+                </TouchableOpacity>
               </View>
               <View>
                  <Icon name='caretright' size={70} color='#ff09de'/>
@@ -41,7 +56,7 @@ export default function Truth({ navigation: { goBack } }) {
         </View>
            
       )}
-      
+  }
       const styles = StyleSheet.create({
         container:{
           backgroundColor: (`#000000`),
